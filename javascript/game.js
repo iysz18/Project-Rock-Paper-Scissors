dba2125245f1c052d23b38c4a8111d6e2f3978ea
Yasin Aydas <115567753+iysz18@users.noreply.github.com>
2023-12-22 20:30:54 +0100

    Added comments describing each functions job, also cleaned up the code

--- javascript/game.js
+++ javascript/game.js
@@ -7,43 +7,44 @@ let playerScore, computerScore;
 playerScore = 0;
 computerScore = 0;
 
+// function that determines winner of a round and returns the winner to the caller
 function playRound(playerSelection, computerSelection) {
     // detect possible tie
     if (playerSelection === computerSelection)  {
         playerScore +=1;
         computerScore +=1;
-        return "Tie detected.";
+        return `Tie! ${playerSelection} & ${computerSelection}`;
     }
     else if (playerSelection === "rock" && computerSelection === "scissors" ||
              playerSelection === "scissors" && computerSelection === "paper" ||
              playerSelection === "paper" && computerSelection === "rock")  {
                 playerScore +=1 ;
-                return `You Won!${playerSelection} beats ${computerSelection}`;
+                return `You Won! ${playerSelection} beats ${computerSelection}.`;
              }
     else {
         computerScore += 1;
-        return `You Lost! ${computerSelection} beats ${playerSelection}`;
+        return `You Lost! ${computerSelection} beats ${playerSelection}.`;
     }
 }
-
+// function to determine the winner of the game, also detect a tie between player and computer score
 function getWinner(playerScore, computerScore) {
     if (playerScore === computerScore) return "Tie between player and computer, play again.";
     else if (playerScore > computerScore) {
-        return `You won! Score: ${playerScore} & ${computerScore}`;
+        return `You won the game!`;
     }
     else {
-        return `You Lost! Score: ${computerScore} & ${playerScore}`;
+        return `You lost the game!`;
     }
 }
 
+// the actual function that runs the game, a set of 5 rounds
 function game() {
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("Enter rock, paper or scissor:", "rock");
         computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
     }
-    console.log(getWinner(playerScore, computerScore));
-
+    console.log(`Game Over! ${getWinner(playerScore, computerScore)}\nScore:\nPlayer: ${playerScore} | Computer: ${computerScore}.`);
 }
 
 game();
